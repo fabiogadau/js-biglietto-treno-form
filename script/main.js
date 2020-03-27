453e15e6a0e8b2fd1fa8ee0860cd733c27072cef
Fabio Gadau <fabiogadau92@gmail.com>
2020-03-27 14:38:29 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,66 @@
+console.log('JS is working!');
+
+/**
+VENDITA BIGLIETTO TRENO
+**/
+ // REFERENZE
+ var container = document.getElementById('biglietto');
+ var bottoneGenera = document.getElementById('bottoneGenera');
+
+ // EVENTI
+ //Generare biglietto
+ bottoneGenera.addEventListener('click',
+  function() {
+    // Ottieni valori input utente
+    var nome = document.getElementById('nome').value;
+    console.log(nome);
+
+    var kmDaPercorrere = document.getElementById('km').value;
+    console.log(kmDaPercorrere);
+
+    var fasciaEta = document.getElementById('fascia-eta').value;
+    console.log(fasciaEta);
+
+    // Calcolo BIGLIETTO
+    var prezzoKm = 0.21;
+    var costoBiglietto = kmDaPercorrere * prezzoKm;
+    var offerta = 'Biglietto Standard';
+
+    // Calcolo costo e offerta applicata
+    if (fasciaEta == 'minorenne') {
+      // 20% sconto
+      costoBiglietto -= costoBiglietto * 0.2;
+      offerta = 'Sconto Minorenne';
+    }
+    else if (fasciaEta == 'over65') {
+      // 40% Sconto
+      costoBiglietto -= costoBiglietto * 0.4;
+      offerta = 'Sconto Over 65';
+    }
+
+    // Controllo dei decimali
+    costoBiglietto = costoBiglietto.toFixed(2);
+
+    // Numero Random per Carrozza da 1 a 9
+    var numCarrozza = Math.floor(Math.random() * 9) + 1;
+
+    // Numero Random per codice cambio prenotazione da 90000 a 100000 escluso
+    var codiceCp = Math.floor(Math.random() * (100000 - 90000) ) + 90000;
+
+    // Inseriamo valori nella pagina
+    document.getElementById('nome-passeggero').innerHTML = nome;
+    document.getElementById('offerta-applicata').innerHTML = offerta;
+    document.getElementById('carrozza').innerHTML = numCarrozza;
+    document.getElementById('codice-cp').innerHTML = codiceCp;
+    document.getElementById('costo').innerHTML = costoBiglietto + '€';
+
+    // Mostra biglietto
+    container.className = 'show';
+
+  }
+
+
+);
+
+// Resetta biglietto
+var bottoneAnnulla = document.getElementById('bottoneAnnulla');
